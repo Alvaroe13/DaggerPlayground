798cb8df1538b70824884efdc45ff720348b7641
Alvaro Isea <alvaroe9448@gmail.com>
2020-09-16 08:39:33 +0200

    done

--- app/src/main/java/com/example/daggerplayground/di/GasolineEngineModule.kt
+++ app/src/main/java/com/example/daggerplayground/di/GasolineEngineModule.kt
@@ -2,16 +2,15 @@ package com.example.daggerplayground.di
 
 import com.example.daggerplayground.model.Engine
 import com.example.daggerplayground.model.GasolineEngine
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class GasolineEngineModule {
+abstract class GasolineEngineModule {
 
     //Here we use Provides and in DieselEngineModule we use @Binds, they're a bit different but here they both work
-    @Provides
-    fun provideGasolineEngine(gasEngine : GasolineEngine) : Engine {
-        return gasEngine
-    }
+    @Binds
+    abstract fun provideGasolineEngine(gasEngine : GasolineEngine) : Engine
 
 }
