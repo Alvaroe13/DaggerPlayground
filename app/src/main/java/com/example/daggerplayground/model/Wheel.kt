3c34b1b8f3e6b6e82c44dd3cd7986bae42301efe
Alvaro Isea <alvaroe9448@gmail.com>
2021-08-18 17:48:27 +0200

    error solved

--- app/src/main/java/com/example/daggerplayground/model/Wheel.kt
+++ app/src/main/java/com/example/daggerplayground/model/Wheel.kt
@@ -7,4 +7,4 @@ package com.example.daggerplayground.model
 class Wheel( screw: Screw, rim: Rim){
     // in this example (branch) we don't have access to this class,
     // we pretend it's from a third party library
-}gi
+}
